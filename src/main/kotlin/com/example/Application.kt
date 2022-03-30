535ee618935f01588848dcf613770509a40d5e04
Claudio Grasso <claudio.grasso@yahoo.com>
2022-03-30 16:24:43 -0300

    Primera versión que anda. Sirve páginas por http y https concertificado de debug.

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,48 @@
+package com.example
+
+import org.slf4j.LoggerFactory
+import io.ktor.application.*
+import io.ktor.network.tls.certificates.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+// import io.ktor.server.netty.*
+import io.ktor.server.jetty.*
+// import io.ktor.server.tomcat.*
+import java.io.*
+
+fun main() {
+    val keyStoreFile = File("build/keystore.jks")
+    val keystore = generateCertificate(
+        file = keyStoreFile,
+        keyAlias = "sampleAlias",
+        keyPassword = "foobar",
+        jksPassword = "foobar"
+    )
+
+    val environment = applicationEngineEnvironment {
+        log = LoggerFactory.getLogger("ktor.application")
+        connector {
+            port = 8080
+        }
+        sslConnector(
+            keyStore = keystore,
+            keyAlias = "sampleAlias",
+            keyStorePassword = { "foobar".toCharArray() },
+            privateKeyPassword = { "foobar".toCharArray() }) {
+            port = 8443
+            keyStorePath = keyStoreFile
+        }
+        module(Application::module)
+    }
+
+    embeddedServer(Jetty, environment).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
